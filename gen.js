cc014e6a781b7e077cb17b7e31de6651f1c01b72
Katya Demidova <demidovakatya@gmail.com>
2015-10-11 01:38:31 +0300

    1 - it works!
    html, css
    txt → js → 3 random lines → html

---
+++ gen.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+    var lines = new Array();
+
+    $.get('source.txt', function (data) {
+        lines = data.split('\n');
+    });
+
+    var x = 0;
+
+    $("#generate-button").click(function () {
+        for (i = 1; i < 4; i++) {
+
+            xold = x;
+            while (xold == x) {
+                // generate 3 random numbers
+                x = Math.floor(Math.random() * lines.length);
+            };
+            $(".result-" + i).text(lines[x]); // use a number instead of x
+        };
+    });
+});
